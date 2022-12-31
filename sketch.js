e232d52ffbfcf328ba578063898568c1d8f40139
Lazy-Coder-03 <53361633+Lazy-Coder-03@users.noreply.github.com>
2023-01-01 00:29:52 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -296,7 +296,10 @@ class BonusFood {
     rect(this.x, this.y, scl, scl);
     //fill(0)
     let x = constrain(this.expirationTime-millis(),0,this.expirationTime)/1000;
+    fill(0);
     textSize(12);
+    stroke(255);
+    ellipse(this.x,this.y,5,5);
     text(nf(x,1,3),this.x,this.y);
   }
 }
